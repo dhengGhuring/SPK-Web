9ac1f1ecdfaca6f4f9898af4f316768fce5e806e
dhengghuring <wyvrendragon@gmail.com>
2023-06-21 00:50:40 +0700

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+// Libaray UI
+import { Button, Center } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <>
+      <Center>
+        <Button colorScheme="blue">Click</Button>
+      </Center>
+    </>
+  );
+}
+
+export default App;
